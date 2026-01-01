309dbab136d72e871570496c811eb04cc7a5c5cd
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2026-01-01 21:07:50 +0700

    simple screen dummy

---
+++ app/src/main/java/com/example/familytracking/ui/screens/HomeScreen.kt
@@ -0,0 +1,34 @@
+package com.example.familytracking.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class HomeScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Home Screen")
+            Button(onClick = { navigator.push(HistoryScreen()) }) {
+                Text("Go to History")
+            }
+            Button(onClick = { navigator.push(ProfileScreen()) }) {
+                Text("Go to Profile")
+            }
+        }
+    }
+}
